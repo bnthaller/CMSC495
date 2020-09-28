cb4cca8e84dba39e60d7202b5413a61288a94958
bnthaller <63174122+bnthaller@users.noreply.github.com>
2020-09-27 18:43:31 -0700

    Delete createSampleDatabase.sql

--- src/createSampleDatabase.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE `topshelf` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE topshelf;
-CREATE TABLE `user` (
-  `username` varchar(16) NOT NULL,
-  `password` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO user (username, password) VALUES('admin','password');
