727de0fedef3cfc1582df75d2c2a5d61ccd19a86
bnthaller <63174122+bnthaller@users.noreply.github.com>
2020-10-02 23:37:06 -0700

    Database Update 2
    Added generic user: user/Password1!
    
    Schema changes as discussed:
    Table: pantry
    
    Columns:
    pantry_id int(11) AI PK
    name varchar(255)
    quantity int(11)
    expiration date
    product_type_id int(11)

--- database/topshelf.sql
+++ database/topshelf.sql
@@ -23,18 +23,15 @@ DROP TABLE IF EXISTS `pantry`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pantry` (
+  `pantry_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `product_type_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `expiration` date NOT NULL,
-  `pantry_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
+  `product_type_id` int(11) NOT NULL,
   PRIMARY KEY (`pantry_id`),
-  KEY `user_id_idx` (`user_id`),
   KEY `product_type_id_idx` (`product_type_id`),
-  CONSTRAINT `product_type_id` FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`product_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `product_type_id` FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`product_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,6 +40,7 @@ CREATE TABLE `pantry` (
 
 LOCK TABLES `pantry` WRITE;
 /*!40000 ALTER TABLE `pantry` DISABLE KEYS */;
+INSERT INTO `pantry` VALUES (1,'Almonds, dry roasted',1,'2020-10-31',8),(2,'Bacon',1,'2020-10-31',1),(3,'Bananas, dehydrated',1,'2020-10-31',8),(4,'Bananas, fresh',1,'2020-10-31',6),(5,'Beans, Black ',1,'2020-10-31',9),(6,'Beans, Pinto',1,'2020-10-31',9),(7,'Beef, Boneless Top Loin Steak',1,'2020-10-31',1),(8,'Beef, Breakfast Sausage',1,'2020-10-31',1),(9,'Beef, Hot Dogs',1,'2020-10-31',1),(10,'Beef, Porterhouse Steak',1,'2020-10-31',1),(11,'Beef, T-Bone Steak',1,'2020-10-31',1),(12,'Beef, Tenderloin Roast',1,'2020-10-31',1),(13,'Beef, Top Round Roast',1,'2020-10-31',1),(14,'Beef, Top Round Steak',1,'2020-10-31',1),(15,'Bread, Wheat',1,'2020-10-31',4),(16,'Bread, White',1,'2020-10-31',4),(17,'Broccoli, raw ',1,'2020-10-31',6),(18,'Butter, Stick, salted',1,'2020-10-31',7),(19,'Butter, Stick, unsalted',1,'2020-10-31',7),(20,'Cantaloupe, raw',1,'2020-10-31',6),(21,'Carrots, sliced or crinkle cut, frozen',1,'2020-10-31',11),(22,'Carrots, sliced or crinkle cut, raw',1,'2020-10-31',6),(23,'Carrots, whole, raw',1,'2020-10-31',6),(24,'Cheese, American, sliced',1,'2020-10-31',7),(25,'Cheese, Cheddar, block',1,'2020-10-31',7),(26,'Cheese, Cheddar, shredded',1,'2020-10-31',7),(27,'Cheese, Cheddar, sliced',1,'2020-10-31',7),(28,'Cheese, Cottage 2% milkfat',1,'2020-10-31',7),(29,'Cheese, Mozzarella, low-moisture',1,'2020-10-31',7),(30,'Cheese, Parmesan, grated',1,'2020-10-31',7),(31,'Cheese, Ricotta',1,'2020-10-31',7),(32,'Cheese, Swiss, sliced',1,'2020-10-31',7),(33,'Chicken, Breast',1,'2020-10-31',1),(34,'Chicken, Drumstick',1,'2020-10-31',1),(35,'Chicken, Thigh',1,'2020-10-31',1),(36,'Chorizo Pork Sausage',1,'2020-10-31',1),(37,'Cookies, Chocolate Chip',1,'2020-10-31',8),(38,'Cookies, Oatmeal',1,'2020-10-31',8),(39,'Egg, Grade A, Large',1,'2020-10-31',7),(40,'Flour, all purpose',1,'2020-10-31',3),(41,'Flour, Corn, Yellow (Fine Meal)',1,'2020-10-31',3),(42,'Flour, Rice (White)',1,'2020-10-31',3),(43,'Flour, White (Enriched) ',1,'2020-10-31',3),(44,'Flour, Whole Wheat',1,'2020-10-31',3),(45,'Ground Turkey, 93% lean',1,'2020-10-31',1),(46,'Haddock, raw',1,'2020-10-31',2),(47,'Ham, sliced, raw',1,'2020-10-31',1),(48,'Ham, whole, raw',1,'2020-10-31',1),(49,'Hummus, Original',1,'2020-10-31',10),(50,'Hummus, Other',1,'2020-10-31',10),(51,'Kale, fresh',1,'2020-10-31',6),(52,'Kale, frozen',1,'2020-10-31',6),(53,'Ketchup',1,'2020-10-31',5),(54,'Kiwi, fresh',1,'2020-10-31',6),(55,'Lettuce, fresh ',1,'2020-10-31',6),(56,'Lettuce, Romaine, fresh ',1,'2020-10-31',6),(57,'Milk, 1%',1,'2020-10-31',7),(58,'Milk, 2%',1,'2020-10-31',7),(59,'Milk, Skim',1,'2020-10-31',7),(60,'Milk, Whole',1,'2020-10-31',7),(61,'Mustard, Deli',1,'2020-10-31',5),(62,'Mustard, Yellow',1,'2020-10-31',5),(63,'Nectarines, fresh',1,'2020-10-31',6),(64,'Oil, Canola',1,'2020-10-31',3),(65,'Oil, Coconut',1,'2020-10-31',3),(66,'Oil, Corn',1,'2020-10-31',3),(67,'Oil, Olive, extra virgin',1,'2020-10-31',3),(68,'Oil, Vegetable',1,'2020-10-31',3),(69,'Olives, Black',1,'2020-10-31',9),(70,'Olives, Green',1,'2020-10-31',9),(71,'Onions, Red',1,'2020-10-31',6),(72,'Onions, Yellow',1,'2020-10-31',6),(73,'Oranges, Navel, fresh',1,'2020-10-31',6),(74,'Peaches, fresh',1,'2020-10-31',6),(75,'Peanut Butter, creamy',1,'2020-10-31',5),(76,'Peanut Butter, crunchy',1,'2020-10-31',5),(77,'Pears, Bosc, fresh',1,'2020-10-31',6),(78,'Pears, Green Anjou, fresh',1,'2020-10-31',6),(79,'Pickles, Kosher Dill, chips',1,'2020-10-31',9),(80,'Pickles, Kosher Dill, spears',1,'2020-10-31',9),(81,'Pollock, raw ',1,'2020-10-31',2),(82,'Pork, Italian Sausage, raw',1,'2020-10-31',1),(83,'Salsa, Chunky, hot',1,'2020-10-31',9),(84,'Salsa, Chunky, medium',1,'2020-10-31',9),(85,'Salt, table, iodized',1,'2020-10-31',3),(86,'Sauce, Pasta, red',1,'2020-10-31',9),(87,'Spinach, fresh ',1,'2020-10-31',6),(88,'Strawberries, fresh',1,'2020-10-31',6),(89,'Strawberries, fresh',1,'2020-10-31',6),(90,'Sugar',1,'2020-10-31',3),(91,'Sunflower Seeds, dry roasted, salted',1,'2020-10-31',8),(92,'Tomatoes, diced, canned',1,'2020-10-31',9),(93,'Tomatoes, Grape',1,'2020-10-31',6),(94,'Tomatoes, whole, canned',1,'2020-10-31',9),(95,'Tuna, canned',1,'2020-10-31',9),(96,'Yogurt, Greek, plain',1,'2020-10-31',7),(97,'Yogurt, Greek, strawberry',1,'2020-10-31',7);
 /*!40000 ALTER TABLE `pantry` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,9 +54,8 @@ DROP TABLE IF EXISTS `product_type`;
 CREATE TABLE `product_type` (
   `product_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `product_type` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`product_type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +64,7 @@ CREATE TABLE `product_type` (
 
 LOCK TABLES `product_type` WRITE;
 /*!40000 ALTER TABLE `product_type` DISABLE KEYS */;
-INSERT INTO `product_type` VALUES (1,'Almonds, dry roasted','Snack'),(2,'Bacon','Meat'),(3,'Bananas, dehydrated','Snack'),(4,'Bananas, fresh','Produce'),(5,'Beans, Black ','Canned'),(6,'Beans, Pinto','Canned'),(7,'Beef, Boneless Top Loin Steak','Meat'),(8,'Beef, Breakfast Sausage','Meat'),(9,'Beef, Hot Dogs','Meat'),(10,'Beef, Porterhouse Steak','Meat'),(11,'Beef, T-Bone Steak','Meat'),(12,'Beef, Tenderloin Roast','Meat'),(13,'Beef, Top Round Roast','Meat'),(14,'Beef, Top Round Steak','Meat'),(15,'Bread, Wheat','Baked Goods'),(16,'Bread, White','Baked Goods'),(17,'Broccoli, raw ','Produce'),(18,'Butter, Stick, salted','Dairy'),(19,'Butter, Stick, unsalted','Dairy'),(20,'Cantaloupe, raw','Produce'),(21,'Carrots, sliced or crinkle cut, frozen','Frozen'),(22,'Carrots, sliced or crinkle cut, raw','Produce'),(23,'Carrots, whole, raw','Produce'),(24,'Cheese, American, sliced','Dairy'),(25,'Cheese, Cheddar, block','Dairy'),(26,'Cheese, Cheddar, shredded','Dairy'),(27,'Cheese, Cheddar, sliced','Dairy'),(28,'Cheese, Cottage 2% milkfat','Dairy'),(29,'Cheese, Mozzarella, low-moisture','Dairy'),(30,'Cheese, Parmesan, grated','Dairy'),(31,'Cheese, Ricotta','Dairy'),(32,'Cheese, Swiss, sliced','Dairy'),(33,'Chicken, Breast','Meat'),(34,'Chicken, Drumstick','Meat'),(35,'Chicken, Thigh','Meat'),(36,'Chorizo Pork Sausage','Meat'),(37,'Cookies, Chocolate Chip','Snack'),(38,'Cookies, Oatmeal','Snack'),(39,'Egg, Grade A, Large','Dairy'),(40,'Flour, all purpose','Baking'),(41,'Flour, Corn, Yellow (Fine Meal)','Baking'),(42,'Flour, Rice (White)','Baking'),(43,'Flour, White (Enriched) ','Baking'),(44,'Flour, Whole Wheat','Baking'),(45,'Ground Turkey, 93% lean','Meat'),(46,'Haddock, raw','Fish'),(47,'Ham, sliced, raw','Meat'),(48,'Ham, whole, raw','Meat'),(49,'Hummus, Original','Refrigerated'),(50,'Hummus, Other','Refrigerated'),(51,'Kale, fresh','Produce'),(52,'Kale, frozen','Produce'),(53,'Ketchup','Condiments'),(54,'Kiwi, fresh','Produce'),(55,'Lettuce, fresh ','Produce'),(56,'Lettuce, Romaine, fresh ','Produce'),(57,'Milk, 1%','Dairy'),(58,'Milk, 2%','Dairy'),(59,'Milk, Skim','Dairy'),(60,'Milk, Whole','Dairy'),(61,'Mustard, Deli','Condiments'),(62,'Mustard, Yellow','Condiments'),(63,'Nectarines, fresh','Produce'),(64,'Oil, Canola','Baking'),(65,'Oil, Coconut','Baking'),(66,'Oil, Corn','Baking'),(67,'Oil, Olive, extra virgin','Baking'),(68,'Oil, Vegetable','Baking'),(69,'Olives, Black','Canned'),(70,'Olives, Green','Canned'),(71,'Onions, Red','Produce'),(72,'Onions, Yellow','Produce'),(73,'Oranges, Navel, fresh','Produce'),(74,'Peaches, fresh','Produce'),(75,'Peanut Butter, creamy','Condiments'),(76,'Peanut Butter, crunchy','Condiments'),(77,'Pears, Bosc, fresh','Produce'),(78,'Pears, Green Anjou, fresh','Produce'),(79,'Pickles, Kosher Dill, chips','Canned'),(80,'Pickles, Kosher Dill, spears','Canned'),(81,'Pollock, raw ','Fish'),(82,'Pork, Italian Sausage, raw','Meat'),(83,'Salsa, Chunky, hot','Canned'),(84,'Salsa, Chunky, medium','Canned'),(85,'Salt, table, iodized','Baking'),(86,'Sauce, Pasta, red','Canned'),(87,'Spinach, fresh ','Produce'),(88,'Strawberries, fresh','Produce'),(89,'Strawberries, fresh','Produce'),(90,'Sugar','Baking'),(91,'Sunflower Seeds, dry roasted, salted','Snack'),(92,'Tomatoes, diced, canned','Canned'),(93,'Tomatoes, Grape','Produce'),(94,'Tomatoes, whole, canned','Canned'),(95,'Tuna, canned','Canned'),(96,'Yogurt, Greek, plain','Dairy'),(97,'Yogurt, Greek, strawberry','Dairy');
+INSERT INTO `product_type` VALUES (1,'Meat'),(2,'Seafood'),(3,'Baking'),(4,'Baked Goods'),(5,'Condiments'),(6,'Produce'),(7,'Dairy'),(8,'Snack'),(9,'Canned'),(10,'Refrigerated'),(11,'Frozen');
 /*!40000 ALTER TABLE `product_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,14 +76,14 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` blob NOT NULL,
   `firstname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `lastname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `expiry_length` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,6 +92,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'user','Password1!','John ','Test',365);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -107,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-29  9:48:37
+-- Dump completed on 2020-10-02 23:27:32
